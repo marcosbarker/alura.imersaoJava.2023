7ea009674278d827e2490b2885f71ce52479f831
marcosbarker <marcos_bareker@hotmail.com>
2023-03-31 16:50:28 -0300

    code: addRedimTextImg

--- src/GeradoraDeStickers.java
+++ src/GeradoraDeStickers.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.InputStream;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 
 public class GeradoraDeStickers {
 
@@ -16,8 +19,8 @@ public class GeradoraDeStickers {
         // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_7.jpg").openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
-        int altura = imagemOriginal.getWidth();
-        int largura = imagemOriginal.getHeight();
+        int altura = imagemOriginal.getHeight();
+        int largura = imagemOriginal.getWidth();
         int novaAltura = altura + 200;
         BufferedImage imagemFinal = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
@@ -28,7 +31,14 @@ public class GeradoraDeStickers {
         graphics2d.setColor(Color.YELLOW);
         graphics2d.setFont(fonte);
 
-        graphics2d.drawString("TESTE", 0, novaAltura - 100);
+        String textoImg = "TESTE";
+        FontMetrics fontMetrics = graphics2d.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(textoImg, graphics2d);
+
+        int larguraTexto = (int) retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+
+        graphics2d.drawString(textoImg, posicaoTextoX, novaAltura - 100);
         ImageIO.write(imagemFinal, "png", new File(nomeArquivo));
     }
 
