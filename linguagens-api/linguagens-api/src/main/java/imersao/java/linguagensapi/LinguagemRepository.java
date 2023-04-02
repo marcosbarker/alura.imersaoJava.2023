05d60070e75d34f7fbaeb5269de28b03d43c1828
marcosbarker <marcos_bareker@hotmail.com>
2023-04-02 16:01:23 -0300

    code: add metodos

---
+++ linguagens-api/linguagens-api/src/main/java/imersao/java/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package imersao.java.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+
+}
